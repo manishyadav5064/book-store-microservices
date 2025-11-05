35ea0e307fbef62bc41839a4d54a107fb9c75ad3
manishyadav5064 <manishyadav5064@gmail.com>
2025-11-06 02:15:27 +0530

    ACAUTOCAS-1001- fixes

--- order-service/src/test/java/com/example/order_service/TestcontainersConfiguration.java
+++ order-service/src/test/java/com/example/order_service/TestcontainersConfiguration.java
@@ -6,20 +6,27 @@ import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.wait.strategy.Wait;
+import java.time.Duration;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
-	}
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    }
 
-	@Bean
-	@ServiceConnection
-	RabbitMQContainer rabbitContainer() {
-		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.12.11-alpine"));
-	}
+    @Bean
+    @ServiceConnection
+    RabbitMQContainer rabbitContainer() {
+        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.12-management"))
+                .withExposedPorts(5672, 15672)
+                .waitingFor(
+                        Wait.forLogMessage(".*Server startup complete.*", 1)
+                                .withStartupTimeout(Duration.ofMinutes(2))
+                );
+    }
 
 }
