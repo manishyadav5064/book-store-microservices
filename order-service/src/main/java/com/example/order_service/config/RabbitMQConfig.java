60f600a4b6859cbc1ca2735cccad93e0dcd81a3e
manishyadav5064 <manishyadav5064@gmail.com>
2025-11-06 01:16:01 +0530

    ACAUTOCAS-1001- order-service first commit

---
+++ order-service/src/main/java/com/example/order_service/config/RabbitMQConfig.java
@@ -0,0 +1,84 @@
+package com.example.order_service.config;
+
+import com.example.order_service.ApplicationProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    private final ApplicationProperties applicationProperties;
+
+    public RabbitMQConfig(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(applicationProperties.orderEventsExchange());
+    }
+
+    @Bean
+    Queue newOrdersQueue() {
+        return QueueBuilder.durable(applicationProperties.newOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding newOrdersQueueBinding() {
+        return BindingBuilder.bind(newOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.newOrdersQueue());
+    }
+
+    @Bean
+    Queue deliveredOrdersQueue() {
+        return QueueBuilder.durable(applicationProperties.deliveredOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding deliveredOrdersQueueBinding() {
+        return BindingBuilder.bind(deliveredOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.deliveredOrdersQueue());
+    }
+
+    @Bean
+    Queue cancelledOrdersQueue() {
+        return QueueBuilder.durable(applicationProperties.cancelledOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding cancelledOrdersQueueBinding() {
+        return BindingBuilder.bind(cancelledOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.cancelledOrdersQueue());
+    }
+
+    @Bean
+    Queue errorOrdersQueue() {
+        return QueueBuilder.durable(applicationProperties.errorOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding errorOrdersQueueBinding() {
+        return BindingBuilder.bind(errorOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.errorOrdersQueue());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, ObjectMapper objectMapper) {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter(objectMapper));
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
---
+++ order-service/src/main/java/com/example/order_service/exception/ApiError.java
@@ -0,0 +1,18 @@
+package com.example.order_service.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+public class ApiError {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private Map<String, String> validationErrors; // only for validation cases
+}
